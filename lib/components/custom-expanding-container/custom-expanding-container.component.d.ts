37b3e3bb2f25029248d072adf206955e471f6a7f
Fatema <fan6236@nyu.edu>
2025-12-17 12:41:00 +0200

    feat: add minExpandingHeight property and update component declaration
    - Added minExpandingHeight property to CustomExpandingContainerComponent.
    - Updated component declaration to include minExpandingHeight in the inputs.
    - Updated version in package.json to reflect changes.

--- lib/components/custom-expanding-container/custom-expanding-container.component.d.ts
+++ lib/components/custom-expanding-container/custom-expanding-container.component.d.ts
@@ -8,13 +8,16 @@ export declare class CustomExpandingContainerComponent {
     backgroundColor: string;
     showBorderBottom: boolean;
     minMaxHeight: string;
+    minExpandingHeight: string;
     toggled: EventEmitter<boolean>;
     isExpanded: import("@angular/core").WritableSignal<boolean>;
     minMaxHeightCss: string;
+    minExpandedHeightCss: string;
+    get isExpandedClass(): boolean;
     ngOnInit(): void;
     toggle(): void;
     open(): void;
     close(): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<CustomExpandingContainerComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<CustomExpandingContainerComponent, "custom-expanding-container", never, { "expanded": { "alias": "expanded"; "required": false; }; "showIcon1": { "alias": "showIcon1"; "required": false; }; "padding": { "alias": "padding"; "required": false; }; "borderRadius": { "alias": "borderRadius"; "required": false; }; "backgroundColor": { "alias": "backgroundColor"; "required": false; }; "showBorderBottom": { "alias": "showBorderBottom"; "required": false; }; "minMaxHeight": { "alias": "minMaxHeight"; "required": false; }; }, { "toggled": "toggled"; }, never, ["[target]", "[content]"], true, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<CustomExpandingContainerComponent, "custom-expanding-container", never, { "expanded": { "alias": "expanded"; "required": false; }; "showIcon1": { "alias": "showIcon1"; "required": false; }; "padding": { "alias": "padding"; "required": false; }; "borderRadius": { "alias": "borderRadius"; "required": false; }; "backgroundColor": { "alias": "backgroundColor"; "required": false; }; "showBorderBottom": { "alias": "showBorderBottom"; "required": false; }; "minMaxHeight": { "alias": "minMaxHeight"; "required": false; }; "minExpandingHeight": { "alias": "minExpandingHeight"; "required": false; }; }, { "toggled": "toggled"; }, never, ["[target]", "[content]"], true, never>;
 }
