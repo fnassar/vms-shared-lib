3a052942214619a2629389c3c2d5725a7529705f
AbdelrahmanSamir <55239369+abdelrahman719@users.noreply.github.com>
2026-01-27 12:16:08 +0200

    deploy for dev and test

---
+++ lib/components/custom-input-form/custom-input-base.d.ts
@@ -0,0 +1,25 @@
+import { EventEmitter } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { IComponentFormError } from '../../interfaces';
+import * as i0 from "@angular/core";
+export declare abstract class CustomInputBase {
+    class: string;
+    labelClass: string;
+    label: string;
+    placeholder: string;
+    name: string;
+    type: 'text' | 'email' | 'password' | 'number';
+    controlName: string;
+    parentForm: FormGroup;
+    validation: IComponentFormError[];
+    pattern: string;
+    height: string;
+    disabled: boolean;
+    valueChange: EventEmitter<string>;
+    containRequiredError(): boolean;
+    ngOnInit(): void;
+    getMaxLength(): number | null;
+    getMinLength(): number | null;
+    static ɵfac: i0.ɵɵFactoryDeclaration<CustomInputBase, never>;
+    static ɵdir: i0.ɵɵDirectiveDeclaration<CustomInputBase, never, never, { "class": { "alias": "class"; "required": false; }; "labelClass": { "alias": "labelClass"; "required": false; }; "label": { "alias": "label"; "required": false; }; "placeholder": { "alias": "placeholder"; "required": false; }; "name": { "alias": "name"; "required": true; }; "type": { "alias": "type"; "required": false; }; "controlName": { "alias": "controlName"; "required": true; }; "parentForm": { "alias": "parentForm"; "required": true; }; "validation": { "alias": "validation"; "required": true; }; "pattern": { "alias": "pattern"; "required": false; }; "height": { "alias": "height"; "required": false; }; "disabled": { "alias": "disabled"; "required": false; }; }, { "valueChange": "valueChange"; }, never, never, true, never>;
+}
