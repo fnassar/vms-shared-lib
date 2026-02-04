d778419fec1d823c319c7d244c678414d0cbb52e
Fatema <fan6236@nyu.edu>
2026-02-04 23:06:00 +0200

    feat(custom-toast): enhance toast component with SVG icons and subText support
    - Added SVG icons for info, success, warning, error, and close actions in custom toast.
    - Updated CustomToastComponent to include SafeHtml properties for SVG icons.
    - Modified ToastService to support subText in toast notifications.
    - Updated package version for new features.

--- lib/components/custom-toast/custom-toast.component.d.ts
+++ lib/components/custom-toast/custom-toast.component.d.ts
@@ -1,10 +1,17 @@
 import { ToastService } from '../../services/ui/toast.service';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import * as i0 from "@angular/core";
 export declare class CustomToastComponent {
     toastService: ToastService;
+    private sanitizer;
     positionClass: string;
     colorClass: string;
-    constructor(toastService: ToastService);
+    infoSvg: SafeHtml;
+    successSvg: SafeHtml;
+    warningSvg: SafeHtml;
+    errorSvg: SafeHtml;
+    closeSvg: SafeHtml;
+    constructor(toastService: ToastService, sanitizer: DomSanitizer);
     private updatePositionClass;
     private updateColorClass;
     hideToast(): void;
--- lib/services/ui/toast.service.d.ts
+++ lib/services/ui/toast.service.d.ts
@@ -4,6 +4,7 @@ export type ToastPosition = 'top-right' | 'top-left' | 'bottom-right' | 'bottom-
 export interface ToastItem {
     id: string;
     message: string;
+    subText: string;
     position: ToastPosition;
     type: ToastType;
     duration: number;
@@ -12,13 +13,14 @@ export interface ToastItem {
 }
 export declare class ToastService {
     message: import("@angular/core").WritableSignal<string>;
+    subText: import("@angular/core").WritableSignal<string>;
     type: import("@angular/core").WritableSignal<ToastType>;
     position: import("@angular/core").WritableSignal<ToastPosition>;
     show: import("@angular/core").WritableSignal<boolean>;
     readonly visibleToasts: import("@angular/core").WritableSignal<ToastItem[]>;
     private readonly maxStackPerPosition;
     private readonly leaveMs;
-    toast(message: string, position: ToastPosition, ToastType?: ToastType, duration?: number): void;
+    toast(message: string, position: ToastPosition, ToastType?: ToastType, duration?: number, subText?: string): void;
     close(id: string): void;
     private normalizeDuration;
     private pushIntoStack;
