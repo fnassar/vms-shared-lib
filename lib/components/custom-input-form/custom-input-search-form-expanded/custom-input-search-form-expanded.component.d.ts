4c68c7eecfd6cd13694958df2bd8f868f26893b6
Fatema <fan6236@nyu.edu>
2026-02-08 02:11:09 +0200

    feat: update CustomFilterDropdownComponent to adjust return type of getDefaultInitialValue method

---
+++ lib/components/custom-input-form/custom-input-search-form-expanded/custom-input-search-form-expanded.component.d.ts
@@ -0,0 +1,46 @@
+import { ElementRef, EventEmitter, OnInit, TemplateRef } from '@angular/core';
+import { IDropdownOption } from '../../../interfaces';
+import { CustomInputBase } from '../custom-input-base';
+import { LoadingService, TranslationService } from '../../../services';
+import * as i0 from "@angular/core";
+interface IPageSearch {
+    nameEn: string;
+    nameAr: string;
+    size: number;
+    page: number;
+}
+export declare class CustomInputSearchFormExpandedComponent<T extends IDropdownOption = IDropdownOption> extends CustomInputBase implements OnInit {
+    toggleSearch: boolean;
+    dropdownTemplate: TemplateRef<{
+        $implicit: any;
+    }>;
+    inputElement?: ElementRef<HTMLInputElement>;
+    dropdownOptionsElement?: ElementRef<HTMLDivElement>;
+    options: import("@angular/core").InputSignal<T[]>;
+    totalSearchOptions: import("@angular/core").InputSignal<number>;
+    pageSearch: import("@angular/core").WritableSignal<IPageSearch>;
+    optionSelected: EventEmitter<T>;
+    viewMoreClicked: EventEmitter<IPageSearch>;
+    searching: import("@angular/core").WritableSignal<boolean>;
+    allowSearch: import("@angular/core").WritableSignal<boolean>;
+    selectedOption: import("@angular/core").WritableSignal<T | null>;
+    private inputSubject;
+    translationService: TranslationService;
+    loadingService: LoadingService;
+    private uniqueId;
+    constructor();
+    ngOnInit(): void;
+    get inputId(): string;
+    onKeyDown(event: KeyboardEvent): void;
+    onInput(): void;
+    toggleAllowSearch(): void;
+    closeDropdown(): void;
+    openDropdown(): void;
+    get clickOutsideElements(): HTMLElement[];
+    get filteredOptions(): T[];
+    selectOption(option: T): void;
+    loadMore(): void;
+    static ɵfac: i0.ɵɵFactoryDeclaration<CustomInputSearchFormExpandedComponent<any>, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<CustomInputSearchFormExpandedComponent<any>, "custom-input-search-expanded-form", never, { "toggleSearch": { "alias": "toggleSearch"; "required": true; }; "dropdownTemplate": { "alias": "dropdownTemplate"; "required": false; }; "options": { "alias": "options"; "required": false; "isSignal": true; }; "totalSearchOptions": { "alias": "totalSearchOptions"; "required": false; "isSignal": true; }; }, { "optionSelected": "optionSelected"; "viewMoreClicked": "viewMoreClicked"; }, never, never, true, never>;
+}
+export {};
