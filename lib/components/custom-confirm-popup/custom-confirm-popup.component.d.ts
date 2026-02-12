604c53a41da6ae00e9e7d126881ec073a0d95ffc
Fatema <fan6236@nyu.edu>
2026-02-12 03:34:50 +0200

    feat: enhance custom confirm popup and sidenav service

--- lib/components/custom-confirm-popup/custom-confirm-popup.component.d.ts
+++ lib/components/custom-confirm-popup/custom-confirm-popup.component.d.ts
@@ -2,10 +2,11 @@ import { EventEmitter } from '@angular/core';
 import { LoadingService } from '../../services';
 import * as i0 from "@angular/core";
 export type ConfirmType = 'confirmation' | 'notifcation';
-export type ConfirmResult = 'confirm' | 'cancel';
+export type ConfirmResult = 'confirm' | 'cancel' | 'close';
 export interface ConfirmOptions {
     modalText: string;
-    modalSubText: string;
+    modalSubText?: string;
+    modalExtraText?: string;
     modalIcon: string;
     type: ConfirmType;
     confirmButtonText?: string;
@@ -20,12 +21,14 @@ export declare class CustomConfirmPopupComponent {
     modalIcon: string;
     modalIconClass: string;
     modalSubText: string;
+    modalExtraText: string;
     confirmButtonText: string;
     confirmButtonStyle: string;
     cancelButtonStyle: string;
     cancelButtonText: string;
     confirmEvent: EventEmitter<any>;
     cancelEvent: EventEmitter<void>;
+    closeEvent: EventEmitter<void>;
     overlayClicked: EventEmitter<boolean>;
     successMsg: import("@angular/core").InputSignal<string>;
     successPressed: boolean;
@@ -38,5 +41,5 @@ export declare class CustomConfirmPopupComponent {
     checkSuccess(): void;
     onOverlayClick(event: MouseEvent): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<CustomConfirmPopupComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<CustomConfirmPopupComponent, "custom-confirm-popup", never, { "modalText": { "alias": "modalText"; "required": true; }; "type": { "alias": "type"; "required": true; }; "modalIcon": { "alias": "modalIcon"; "required": false; }; "modalIconClass": { "alias": "modalIconClass"; "required": false; }; "modalSubText": { "alias": "modalSubText"; "required": false; }; "confirmButtonText": { "alias": "confirmButtonText"; "required": false; }; "confirmButtonStyle": { "alias": "confirmButtonStyle"; "required": false; }; "cancelButtonStyle": { "alias": "cancelButtonStyle"; "required": false; }; "cancelButtonText": { "alias": "cancelButtonText"; "required": false; }; "successMsg": { "alias": "successMsg"; "required": false; "isSignal": true; }; }, { "confirmEvent": "confirmEvent"; "cancelEvent": "cancelEvent"; "overlayClicked": "overlayClicked"; "closed": "closed"; }, never, never, true, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<CustomConfirmPopupComponent, "custom-confirm-popup", never, { "modalText": { "alias": "modalText"; "required": true; }; "type": { "alias": "type"; "required": true; }; "modalIcon": { "alias": "modalIcon"; "required": false; }; "modalIconClass": { "alias": "modalIconClass"; "required": false; }; "modalSubText": { "alias": "modalSubText"; "required": false; }; "modalExtraText": { "alias": "modalExtraText"; "required": false; }; "confirmButtonText": { "alias": "confirmButtonText"; "required": false; }; "confirmButtonStyle": { "alias": "confirmButtonStyle"; "required": false; }; "cancelButtonStyle": { "alias": "cancelButtonStyle"; "required": false; }; "cancelButtonText": { "alias": "cancelButtonText"; "required": false; }; "successMsg": { "alias": "successMsg"; "required": false; "isSignal": true; }; }, { "confirmEvent": "confirmEvent"; "cancelEvent": "cancelEvent"; "closeEvent": "closeEvent"; "overlayClicked": "overlayClicked"; "closed": "closed"; }, never, never, true, never>;
 }
--- lib/services/sideNav/side-nav-collapse.service.d.ts
+++ lib/services/sideNav/side-nav-collapse.service.d.ts
@@ -1,9 +1,12 @@
 import * as i0 from "@angular/core";
 export declare class SidenavService {
     private _isCollapsed;
+    private _hidden;
     constructor();
     get isCollapsed(): boolean;
+    get hidden(): boolean;
     toggle(): void;
+    toggleHide(): void;
     collapse(): void;
     expand(): void;
     private listenToWindowResize;
