697219b0a5ff031ad4db7abd8e79a26134ea1512
Fatema <fan6236@nyu.edu>
2026-02-19 10:38:05 +0200

    feat: add height property to CustomSearchInputComponent and update version in package.json

--- lib/components/custom-search-input/custom-search-input.component.d.ts
+++ lib/components/custom-search-input/custom-search-input.component.d.ts
@@ -8,7 +8,8 @@ export declare class CustomSearchInputComponent {
     name: string;
     value: any;
     style: 'base' | 'white' | 'grey';
+    height: string;
     valueChange: EventEmitter<any>;
     static ɵfac: i0.ɵɵFactoryDeclaration<CustomSearchInputComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<CustomSearchInputComponent, "custom-search-input", never, { "class": { "alias": "class"; "required": false; }; "labelClass": { "alias": "labelClass"; "required": false; }; "label": { "alias": "label"; "required": false; }; "placeholder": { "alias": "placeholder"; "required": false; }; "name": { "alias": "name"; "required": false; }; "value": { "alias": "value"; "required": true; }; "style": { "alias": "style"; "required": false; }; }, { "valueChange": "valueChange"; }, never, never, true, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<CustomSearchInputComponent, "custom-search-input", never, { "class": { "alias": "class"; "required": false; }; "labelClass": { "alias": "labelClass"; "required": false; }; "label": { "alias": "label"; "required": false; }; "placeholder": { "alias": "placeholder"; "required": false; }; "name": { "alias": "name"; "required": false; }; "value": { "alias": "value"; "required": true; }; "style": { "alias": "style"; "required": false; }; "height": { "alias": "height"; "required": false; }; }, { "valueChange": "valueChange"; }, never, never, true, never>;
 }
