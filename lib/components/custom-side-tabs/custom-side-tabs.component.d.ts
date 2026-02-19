73b6eb7a14c833fbee7fab5b089bda0142a9888e
Fatema <fan6236@nyu.edu>
2026-02-19 02:38:03 +0200

    feat: add desktop detection and tab retrieval method to CustomSideTabsComponent
    - Added `isDesktop` property to track desktop view.
    - Implemented `onResize` method for handling window resize events.
    - Introduced `getTab` method to retrieve a tab by its ID.
    - Updated version in package.json to reflect changes.

--- lib/components/custom-side-tabs/custom-side-tabs.component.d.ts
+++ lib/components/custom-side-tabs/custom-side-tabs.component.d.ts
@@ -8,10 +8,13 @@ export declare class CustomSideTabsComponent {
     activeId: string | number;
     lang: 'en' | 'ar';
     activeIdChange: EventEmitter<string | number>;
+    isDesktop: boolean;
+    onResize(): void;
     constructor(sanitizer: DomSanitizer);
     getSafeSvg(svg: string | undefined): SafeHtml | null;
     selectTab(tab: ITabDropdownOption): void;
     getLabel(tab: ITabDropdownOption): string;
+    getTab(id: string | number): ITabDropdownOption;
     static ɵfac: i0.ɵɵFactoryDeclaration<CustomSideTabsComponent, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<CustomSideTabsComponent, "custom-side-tabs", never, { "tabs": { "alias": "tabs"; "required": false; }; "activeId": { "alias": "activeId"; "required": false; }; "lang": { "alias": "lang"; "required": false; }; }, { "activeIdChange": "activeIdChange"; }, never, ["*"], true, never>;
 }
