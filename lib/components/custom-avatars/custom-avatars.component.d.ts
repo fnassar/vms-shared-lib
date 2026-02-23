77c986ee59edf21b8792805a1d5dc54f9c30df9f
Fatema <fan6236@nyu.edu>
2026-02-23 10:14:58 +0200

    feat(avatars): enhance CustomAvatarsComponent with translation service and new color methods
    - Added TranslationService import to CustomAvatarsComponent.
    - Modified getRandomColor method to accept a name parameter.
    - Introduced getRandomColorForArabic method for handling Arabic names.
    - Updated package version to reflect changes.

--- lib/components/custom-avatars/custom-avatars.component.d.ts
+++ lib/components/custom-avatars/custom-avatars.component.d.ts
@@ -1,3 +1,4 @@
+import { TranslationService } from '../../services';
 import * as i0 from "@angular/core";
 export interface AvatarItem {
     imgPath?: string;
@@ -9,8 +10,10 @@ export declare class CustomAvatarsComponent {
     overlapOffset: number;
     removeOutline: boolean;
     removeBorder: boolean;
+    translationService: TranslationService;
     getInitials(name: string): string;
-    getRandomColor(): string;
+    getRandomColor(name: string): string;
+    getRandomColorForArabic(name: string): string;
     static ɵfac: i0.ɵɵFactoryDeclaration<CustomAvatarsComponent, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<CustomAvatarsComponent, "custom-avatars", never, { "items": { "alias": "items"; "required": false; }; "size": { "alias": "size"; "required": false; }; "overlapOffset": { "alias": "overlapOffset"; "required": false; }; "removeOutline": { "alias": "removeOutline"; "required": false; }; "removeBorder": { "alias": "removeBorder"; "required": false; }; }, {}, never, never, true, never>;
 }
