735e20da0e61d5fa9b058bb2de67c41513e0911b
Fatema <fan6236@nyu.edu>
2026-02-25 13:58:58 +0200

    feat: enhance CustomSearchInputComponent with lifecycle hooks and search emission functionality
    - Implemented OnInit and OnDestroy interfaces in CustomSearchInputComponent.
    - Added private searchSubject for managing search input.
    - Introduced emitSearch method to handle search value emissions.
    - Updated component declaration in TypeScript definition file.
    
    chore: update package version to 0.0.0-watch+1772020620093

--- lib/components/custom-search-input/custom-search-input.component.d.ts
+++ lib/components/custom-search-input/custom-search-input.component.d.ts
@@ -1,6 +1,6 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, OnDestroy, OnInit } from '@angular/core';
 import * as i0 from "@angular/core";
-export declare class CustomSearchInputComponent {
+export declare class CustomSearchInputComponent implements OnInit, OnDestroy {
     class: string;
     labelClass: string;
     label: string;
@@ -10,6 +10,10 @@ export declare class CustomSearchInputComponent {
     style: 'base' | 'white' | 'grey';
     height: string;
     valueChange: EventEmitter<any>;
+    private searchSubject;
+    ngOnInit(): void;
+    ngOnDestroy(): void;
+    emitSearch(value: string): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<CustomSearchInputComponent, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<CustomSearchInputComponent, "custom-search-input", never, { "class": { "alias": "class"; "required": false; }; "labelClass": { "alias": "labelClass"; "required": false; }; "label": { "alias": "label"; "required": false; }; "placeholder": { "alias": "placeholder"; "required": false; }; "name": { "alias": "name"; "required": false; }; "value": { "alias": "value"; "required": true; }; "style": { "alias": "style"; "required": false; }; "height": { "alias": "height"; "required": false; }; }, { "valueChange": "valueChange"; }, never, never, true, never>;
 }
