deb07e43b60882169bb3e4b2bc153d0d9cd9672f
Fatema <fan6236@nyu.edu>
2026-04-26 23:51:08 +0300

    feat: enhance CustomPlateNumberInputComponent with additional formatting and input handling methods
    - Added formatWithSpaces method for better plate number formatting.
    - Reintroduced splitPlateNumber, onLettersInput, and onDigitsInput methods for improved input handling.
    - Updated package version to 0.0.0-watch+1777236619065.

--- lib/components/custom-input-form/custom-plate-number-input/custom-plate-number-input.component.d.ts
+++ lib/components/custom-input-form/custom-plate-number-input/custom-plate-number-input.component.d.ts
@@ -8,12 +8,13 @@ export declare class CustomPlateNumberInputComponent extends CustomInputBase {
     lettersValue: string;
     digitsValue: string;
     ngOnInit(): void;
-    splitPlateNumber(value: string): void;
-    onLettersInput(event: Event): void;
-    onDigitsInput(event: Event): void;
     preventInvalidInput(event: KeyboardEvent, type: 'letters' | 'digits'): void;
     isControlKey(event: KeyboardEvent): boolean;
     updateFormValue(): void;
+    formatWithSpaces(value: string): string;
+    splitPlateNumber(value: string): void;
+    onLettersInput(event: Event): void;
+    onDigitsInput(event: Event): void;
     getCombinedValue(): string;
     static ɵfac: i0.ɵɵFactoryDeclaration<CustomPlateNumberInputComponent, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<CustomPlateNumberInputComponent, "custom-plate-number-input", never, { "maxLetters": { "alias": "maxLetters"; "required": false; }; "maxDigits": { "alias": "maxDigits"; "required": false; }; "placeholderLetters": { "alias": "placeholderLetters"; "required": false; }; "placeholderDigits": { "alias": "placeholderDigits"; "required": false; }; }, {}, never, never, true, never>;
