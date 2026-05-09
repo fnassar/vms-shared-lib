d0df1f87fccba692913955340398b1b5ac2acb6a
Fatema <fan6236@nyu.edu>
2026-05-09 19:14:26 +0300

    feat(tooltip): enhance CustomTooltipComponent with tooltipStyle and position calculation
    - Added tooltipStyle property to allow custom styling of the tooltip.
    - Introduced private el property for element reference.
    - Implemented onScrollOrResize method to handle scroll and resize events.
    - Added calculatePosition method for dynamic tooltip positioning.
    
    chore(package): update version to 0.0.0-watch+1778343199548

--- lib/components/custom-tooltip/custom-tooltip.component.d.ts
+++ lib/components/custom-tooltip/custom-tooltip.component.d.ts
@@ -10,9 +10,13 @@ export declare class CustomTooltipComponent {
     templateContext?: any;
     minWidth: string;
     visible: boolean;
+    tooltipStyle: Record<string, string>;
     private timeoutId;
+    private readonly el;
+    onScrollOrResize(): void;
     show(): void;
     hide(): void;
+    private calculatePosition;
     static ɵfac: i0.ɵɵFactoryDeclaration<CustomTooltipComponent, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<CustomTooltipComponent, "custom-tooltip", never, { "customClass": { "alias": "customClass"; "required": false; }; "text": { "alias": "text"; "required": false; }; "title": { "alias": "title"; "required": false; }; "delay": { "alias": "delay"; "required": false; }; "position": { "alias": "position"; "required": false; }; "template": { "alias": "template"; "required": false; }; "templateContext": { "alias": "templateContext"; "required": false; }; "minWidth": { "alias": "minWidth"; "required": false; }; }, {}, never, ["*"], true, never>;
 }
